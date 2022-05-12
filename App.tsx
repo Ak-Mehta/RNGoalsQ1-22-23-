6487834f9fbf1700e565c60dcadd9c4acee59fee
Ak-Mehta <akshay.mehta@crownstack.com>
2022-05-12 15:20:20 +0530

    reanimated

--- App.tsx
+++ App.tsx
@@ -10,13 +10,10 @@
 
 import React from 'react';
 import {Text, View} from 'react-native';
+import Reanime from './Reanime';
 
 const App = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello Anime!</Text>
-    </View>
-  );
+  return <Reanime />;
 };
 
 export default App;
