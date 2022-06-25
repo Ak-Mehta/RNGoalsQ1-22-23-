57e8af82a48c7d71b35b0f0d852d35e1826f7184
Ak-Mehta <akshay.mehta@crownstack.com>
2022-06-25 10:35:24 +0530

    push notify

--- App.tsx
+++ App.tsx
@@ -8,12 +8,44 @@
  * @format
  */
 
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, Text, View} from 'react-native';
+import FlashMessage from 'react-native-flash-message';
+import {requestUserPermission} from './src/notificationServices';
+import messaging, {firebase} from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+import {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
 
 const App = () => {
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+  useEffect(() => {
+    requestUserPermission();
+
+    firebase.messaging().onMessage(remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+
+      // PushNotification.localNotification({
+      //   channelId: 'akk',
+      //   title: remoteMessage.notification?.title,
+      //   message: remoteMessage.notification?.body!,
+      // });
+      showNotification(remoteMessage.notification!);
+    });
+  }, []);
+  const showNotification = (
+    notification: FirebaseMessagingTypes.Notification,
+  ) => {
+    PushNotification.localNotification({
+      channelId: 'ak',
+      title: notification.title,
+      message: notification.body!,
+    });
+  };
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <FlashMessage position="top" />
       <Text>Hello Anime!</Text>
     </View>
   );
---
+++ src/notificationServices.ts
@@ -0,0 +1,34 @@
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {showError, showSuccess} from './helper';
+
+//requesting ios permission
+export const requestUserPermission = async () => {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    getFcmToken();
+  }
+};
+
+export const getFcmToken = async () => {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  console.log(fcmToken, 'old token');
+
+  if (!fcmToken) {
+    try {
+      const fcmToken = await messaging().getToken();
+      if (fcmToken) {
+        console.log(fcmToken, 'new token');
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    } catch (error: any) {
+      console.log(error, 'fcmToken error');
+      showError(error.message);
+    }
+  }
+};
